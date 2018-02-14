38c4ebe11264c043fc143fa00e6c9334221e60d0
Ana Sofia Saenz <30756121+chofia@users.noreply.github.com>
2018-02-14 16:55:12 -0500

    Add files via upload

---
+++ overlay_menu/overlay.js
@@ -0,0 +1,12 @@
+//Toggle for width display
+
+var showWidth = 1;
+        
+    if (showWidth == 1 ) {
+        $(document).ready(function() {
+            $(window).resize(function() {
+                var width = $(window).width();
+                    document.getElementById('output_width').innerHTML= "Window Width: " + width.toString();
+            })
+        })
+    }
